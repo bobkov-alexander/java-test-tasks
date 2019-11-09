d0be19999e2c05249f72a6d5f076eec176b6fe3d
Alexander Bobkov <bobkov.alexander.18@gmail.com>
2019-11-09 20:50:35 +0300

    Added logic for task 4

--- src/com/bobkov/task4/SubstringCounter.java
+++ src/com/bobkov/task4/SubstringCounter.java
@@ -1,4 +1,31 @@
 package com.bobkov.task4;
 
+import java.util.Scanner;
+
+
 public class SubstringCounter {
+
+    public static int countMatches(String text, String searchWord) {
+        text = text.toLowerCase();
+        searchWord = searchWord.toLowerCase();
+        int counter = 0;
+        while (text.contains(searchWord)) {
+            counter++;
+            int position = text.indexOf(searchWord) + 1;
+            if (position >= text.length()) {
+                break;
+            }
+            text = text.substring(position);
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter text: ");
+        String text = scanner.nextLine();
+        System.out.print("Enter a search word: ");
+        String word = scanner.nextLine();
+        System.out.println("Number of matches: " + countMatches(text, word));
+    }
 }
